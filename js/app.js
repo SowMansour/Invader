84abacba7ae6412792758b873194ce90d0a41e53
SowMansour <mansoursow10@gmail.com>
2023-08-21 11:55:56 +0200

    Step 1: Canvas Creation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// ton code ici
+const container = document.getElementById('invader');
+
+for(let i = 0; i < 8*8 ; i++){
+    // Creation des elements invader et stylisation
+    const pixelDiv = document.createElement('div');
+    pixelDiv.classList.add('cell');
+    //Append the the row div to an existing elem(here invader)
+    container.appendChild(pixelDiv);
+}
