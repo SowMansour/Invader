01153f1f493c8d16c90c8f2dee2ec2f67a4387dc
SowMansour <mansoursow10@gmail.com>
2023-08-21 15:57:21 +0200

    Add Event & Form

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
+// Par défaut la grille sera de 8
+const gridInit = function(gridSize = 8){
 const container = document.getElementById('invader');
-
-for(let i = 0; i < 8*8 ; i++){
+// On initialise la grille
+container.textContent = '';
+// On adapte l'affichage pour le style s'adapte à n'importe quelle chiffre
+container.style.width = `${5*gridSize}rem`;
+for(let i = 0; i < gridSize*gridSize ; i++){
     // Creation des elements invader et stylisation
-    const pixelDiv = document.createElement('div');
+    let pixelDiv = document.createElement('div');
     pixelDiv.classList.add('cell');
     //Append the the row div to an existing elem(here invader)
     container.appendChild(pixelDiv);
-}
+
+    pixelDiv.addEventListener('click', ()=>{
+        pixelDiv.classList.toggle('black-color');
+    });
+};
+};
+
+const canvaForm = document.querySelector('.configuration');
+//Create form elem
+const canvasSize = document.createElement('input');
+canvasSize.classList.add('canvasSizeS');
+canvasSize.placeholder = "Taille de la grille";
+canvasSize.type = 'number';
+
+const btn = document.createElement('button');
+btn.classList.add('btnStyle');
+btn.textContent = "Valider";
+
+//Apprend elems to parent
+canvaForm.appendChild(canvasSize);
+canvaForm.appendChild(btn);
+
+canvaForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+// Récupère la valeur rentré par le user
+    gridInit(canvasSize.valueAsNumber);
+});
+
+gridInit();
+
+
